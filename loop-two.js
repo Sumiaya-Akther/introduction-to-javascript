df0d73036a45ea9bd0b0ab42556fcdc47746a0e1
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-02-10 00:48:31 +0600

    new

--- loop-two.js
+++ loop-two.js
@@ -81,12 +81,28 @@
 // }
 
 
-let n = 1;
-while (n < 15) {
-    n++;
-    if (n % 2 !== 1) {
-        continue;
-    }
-    console.log(n);
+// let n = 1;
+// while (n < 15) {
+//     n++;
+//     if (n % 2 !== 1) {
+//         continue;
+//     }
+//     console.log(n);
     
+// }
+
+
+//do while loop--------->
+
+// almost use korbo na
+// let i = 10;
+// while (i < 5) {
+//     console.log(i);
+//     i++
+// }
+let n = 0;
+do {
+    console.log(n);
+    n++;
 }
+while (n < 5)
